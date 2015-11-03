7d2c7c1b07eb8bb79a443bddf512c0a357b5dd04
Gabriel C <Gabriel.Case@RACKSPACE.COM>
2015-11-03 16:44:32 -0600

    added a python script for formatting results into a table

---
+++ report.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import time
+from pprint import pprint
+from zapv2 import ZAPv2
+from collections import defaultdict
+from prettytable import PrettyTable
+
+zap = ZAPv2()
+zap = ZAPv2(proxies={'http': 'http://127.0.0.1:8500', 'https': 'http://127.0.0.1:8500'})
+alerts = zap.core.alerts()
+#pprint (zap.core.alerts())
+
+# create a data structure to match our output
+sort_by_url = defaultdict(list)
+for alert in alerts:
+    sort_by_url[alert['url']].append({
+                                'risk':  alert['risk'],
+                                'alert': alert['alert']
+                                })
+
+# print a useful set of tables of the alerts
+for url in sort_by_url:
+    print
+    print url
+
+    results = PrettyTable(["Risk", "Description"])
+    results.padding_width = 1
+    results.align = "l"
+    results.sortby = "Risk"
+
+    for details in sort_by_url[url]:
+        results.add_row([details['risk'], details['alert']])
+    print results
